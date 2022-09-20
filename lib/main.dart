1f29d3eb142a1251ba94f1f2182518cfdb5fb6ce
zandkhan <zand.private@gmail.com>
2022-09-20 16:47:01 +0430

    First commit project in github and add file to repository

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //hide mode banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am happy'),
+        ),
+        //This widget is to display the elements in the center of the page
+        body: const Center(
+          //This widget is to display the picture
+          child: Image(
+            //Fully cover the frame
+            fit: BoxFit.contain ,
+            image: AssetImage('images/happy.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
